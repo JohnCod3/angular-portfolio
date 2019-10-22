def94f0c6eef457c5dba752623712db425ea4cd1
John Fabio Giraldo <codigojohn@gmail.com>
2019-10-22 01:35:00 -0500

    Template listo sin rutas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		FooterComponent,
+		HeaderComponent,
+		PortfolioComponent
+	],
+	imports: [
+		BrowserModule
+	],
+	providers: [],
+	bootstrap: [
+		AppComponent
+	]
 })
-export class AppModule { }
+
+export class AppModule {}
