b94041ddd6ef5350d9198d7bcc69ea6065fcbb64
John Fabio Giraldo <codigojohn@gmail.com>
2019-10-22 02:49:02 -0500

    Finalizadas las rutas básicas.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+
+const routes: Routes = [
+	{ path: 'home', component: PortfolioComponent },
+	{ path: 'item', component: ItemComponent },
+	{ path: 'about', component: AboutComponent },
+	{ path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
+
+@NgModule({
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AboutComponent } from './pages/about/about.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { ItemComponent } from './pages/item/item.component';
 import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
+		AboutComponent,
 		FooterComponent,
 		HeaderComponent,
+		ItemComponent,
 		PortfolioComponent
 	],
 	imports: [
+		AppRoutingModule,
 		BrowserModule
 	],
 	providers: [],
